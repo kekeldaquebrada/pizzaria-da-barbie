1b6ac626ad179a65d5e852d847f6b1bbc4de4070
kekeldaquebrada <124931192+kekeldaquebrada@users.noreply.github.com>
2023-03-20 15:58:01 -0300

    mudeiphp

--- index.php
+++ index.php
@@ -14,10 +14,11 @@
     ?>
     <?php 
     $servername="localhost";
-    $username="PizzariadaBarbie";
-    $password="barbie123";
+    $username="id20480765_barbiemodaemagia";
+    $password="Barb!e123456";
+    $nomedb="id20480765_pizzariadabarbie";
     //cria conexão
-    $conn=new mysqli($servername, $username, $password);
+    $conn=new mysqli($servername, $username, $password,$nomedb);
     //testa conexão
     if ($conn->connect_error){
         die("conexão com db falhou:".$conn->connect_error);
