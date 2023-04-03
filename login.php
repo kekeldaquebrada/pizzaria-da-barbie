44492e49c0fa9da797d1ea504548423e39b7bbab
kekeldaquebrada <124931192+kekeldaquebrada@users.noreply.github.com>
2023-04-03 16:56:05 -0300

    aaaaaaaa

--- login.php
+++ login.php
@@ -9,30 +9,13 @@
 </head>
 <body>
     <h1>Fazer Login</h1>
-<<<<<<< HEAD
+
     <form action="login.php" method="post">
-  <div class="imgcontainer">
-    
-  </div>
-
-  <div class="container">
-    <label for="uname"><b>Usuario</b></label>
-    <input type="text" placeholder="Enter Username" name="fusuario" required>
-
-    <label for="psw"><b>Senha</b></label>
-    <input type="password" placeholder="Enter Password" name="fsenha" required>
+    <label> Usuario</label><br>
+        <input type="text" name="fusuario"><br>
+        <label> Senha</label><br>
+        <input type="text" name="fsenha"><br>
         
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
-
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancelar</button>
-    <span class="psw">Forgot <a href="#">senha?</a></span>
-  </div>
-</form>
     <a href="index.php">Consultar agenda </a>
     <?php
     echo "Fazer Login"
