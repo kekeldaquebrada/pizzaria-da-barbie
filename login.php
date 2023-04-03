fec9ba9903fdbede05c8f11d801315966d3c5c08
kekeldaquebrada <124931192+kekeldaquebrada@users.noreply.github.com>
2023-04-03 15:33:56 -0300

    <><>,.,

--- login.php
+++ login.php
@@ -5,8 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
+    <h1>Fazer Login</h1>
+    <form action="login.php">
+        <Label>Usuario</Label>
+        <input type="text" name="fusuario"><br>
+        <Label>Senha</Label>
+        <input type="text" name="fsenha"><br>
     <a href="index.php">Consultar agenda </a>
     <?php
     echo "Fazer Login"
