72c4b06d6373547139f3032a25d5a9be7e799503
kekeldaquebrada <124931192+kekeldaquebrada@users.noreply.github.com>
2023-04-04 17:05:04 -0300

    ...

--- login.php
+++ login.php
@@ -21,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION["user_id"] = $row["id"];
 
         // Redireciona para a página de cadastro
-        header("Location: contato.php");
+        echo "<meta http-equiv='refresh'content='0'/>";
         exit();
     } else {
         // Exibe uma mensagem de erro caso o usuário e senha não correspondam a um registro
