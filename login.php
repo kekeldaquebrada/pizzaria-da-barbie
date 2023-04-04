54b7c491c981ea417f24c4f5b5108d3c0d96feb9
kekeldaquebrada <124931192+kekeldaquebrada@users.noreply.github.com>
2023-04-04 15:21:39 -0300

    cvcvcvcvcvcv

--- login.php
+++ login.php
@@ -23,6 +23,35 @@
     ?>
     <?php
     include "conexao.php";
+
+    // Inicializa a sessão
+session_start();
+
+// Verifica se o formulário de login foi submetido
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtém o nome de usuário e senha do formulário
+    $username = mysqli_real_escape_string($conn, $_POST["username"]);
+    $password = mysqli_real_escape_string($conn, $_POST["password"]);
+
+    // Consulta o banco de dados para verificar se o usuário e senha correspondem a um registro
+    $sql = "SELECT id FROM usuarios WHERE nome = '$username' AND senha = '$password'";
+    $result = mysqli_query($conn, $sql);
+
+    // Verifica se a consulta retornou algum resultado
+    if (mysqli_num_rows($result) == 1) {
+        // Inicia a sessão com o ID do usuário
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION["user_id"] = $row["id"];
+
+        // Redireciona para a página de cadastro
+        header("Location: cadastro.php");
+        exit();
+    } else {
+        // Exibe uma mensagem de erro caso o usuário e senha não correspondam a um registro
+        $login_error = "Nome de usuário ou senha inválidos.";
+    }
+}
+
     ?>
 </body>
 </html>
