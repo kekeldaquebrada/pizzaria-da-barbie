2fa7dada20bc0e1694fae14ad92951ebc4dd1986
kekeldaquebrada <124931192+kekeldaquebrada@users.noreply.github.com>
2023-04-05 15:44:59 -0300

    estilo.css

--- login.php
+++ login.php
@@ -1,17 +1,18 @@
 <?php
-    include "conexao.php";
 
-    // Inicializa a sessão
-//session_start();
+include "conexao.php";
+
+// Inicializa a sessão
+session_start();
 
 // Verifica se o formulário de login foi submetido
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obtém o nome de usuário e senha do formulário
-    $usuario = mysqli_real_escape_string($conn, $_POST["fusuario"]);
-    $senha = mysqli_real_escape_string($conn, $_POST["fsenha"]);
+    $username = mysqli_real_escape_string($conn, $_POST["username"]);
+    $password = mysqli_real_escape_string($conn, $_POST["password"]);
 
     // Consulta o banco de dados para verificar se o usuário e senha correspondem a um registro
-    $sql = "SELECT id FROM usuarios2 WHERE nome = '$usuario' AND senha = '$senha'";
+    $sql = "SELECT id FROM usuarios WHERE nome = '$username' AND senha = '$password'";
     $result = mysqli_query($conn, $sql);
 
     // Verifica se a consulta retornou algum resultado
@@ -20,8 +21,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $row = mysqli_fetch_assoc($result);
         $_SESSION["user_id"] = $row["id"];
 
-        // Redireciona para a página de cadastro.
-        echo "<meta http-equiv='contato.php'content='0'/>";
+        // Redireciona para a página de cadastro
+        header("Location: cadastro.php");
         exit();
     } else {
         // Exibe uma mensagem de erro caso o usuário e senha não correspondam a um registro
@@ -29,30 +30,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-    ?>
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
     <link rel="stylesheet" href="estilo.css">
+    <title>Faça seu login</title>
 </head>
 <body>
-    <h1>Fazer Login</h1>
 
-    <form action=""method="post">
-    <label> Usuario</label><br>
-        <input type="text" name="fusuario"><br>
-        <label> Senha</label><br>
-        <input type="password" name="fsenha"><br>
-        <input type="submit" value="Entrar">
-        </form>
-    <a href="index.php">Consultar agenda </a>
-    <?php
-    echo "Fazer Login"
-    ?>
-    
+<h1>Login</h1>
+<?php if (isset($login_error)) { ?>
+    <p><?php echo $login_error; ?></p>
+<?php } ?>
+<form action="" method="post">
+    <label for="username">Nome de usuário:</label>
+    <input type="text" name="username" required>
+    <br>
+    <label for="password">Senha:</label>
+    <input type="password" name="password" required>
+    <br>
+    <input type="submit" value="Entrar">
+</form>
+
 </body>
 </html>
